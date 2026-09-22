92aff3f26e764ed85c27fd3389b41cad510b0052
Mehmed <mehmedm325@gmail.com>
2026-09-22 20:18:06 +0300

    Add SqlGenerator binding to AskSqlServiceProvider for AnthropicSqlGenerator integration

--- src/AskSqlServiceProvider.php
+++ src/AskSqlServiceProvider.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace AskSql\AskSql;
 
 use AskSql\AskSql\Console\Commands\AskSqlCommand;
+use AskSql\AskSql\Contracts\SqlGenerator;
+use AskSql\AskSql\Generators\AnthropicSqlGenerator;
 use Illuminate\Support\ServiceProvider;
 
 class AskSqlServiceProvider extends ServiceProvider
@@ -17,6 +19,8 @@ class AskSqlServiceProvider extends ServiceProvider
         $this->mergeConfigFrom(__DIR__.'/../config/asksql.php', 'asksql');
 
         $this->app->singleton(AskSql::class);
+
+        $this->app->singleton(SqlGenerator::class, AnthropicSqlGenerator::class);
     }
 
     /**
